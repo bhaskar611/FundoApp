64314e17c7beba0a1a9094259fedd39ca20286a9
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-04-26 10:41:29 +0530

    Added:forgot password method

--- app/src/main/java/com/example/fundoapp/LoginActivity.java
+++ app/src/main/java/com/example/fundoapp/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.fundoapp;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,9 +10,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -19,9 +23,9 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText emailId, password;
+    EditText emailId, passwordId;
     Button btnSignIn;
-    TextView tvSignUp;
+    TextView textViewvSignUp,forgotpassword;
     FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
 
@@ -32,9 +36,10 @@ public class LoginActivity extends AppCompatActivity {
 
         mFirebaseAuth = FirebaseAuth.getInstance();
         emailId = findViewById(R.id.editText);
-        password = findViewById(R.id.editText2);
+        passwordId = findViewById(R.id.editText2);
         btnSignIn = findViewById(R.id.button2);
-        tvSignUp = findViewById(R.id.textView);
+        textViewvSignUp = findViewById(R.id.textView);
+        forgotpassword = findViewById(R.id.textView3);
 
         mAuthStateListener = firebaseAuth -> {
             FirebaseUser mFirebaseUser = mFirebaseAuth.getCurrentUser();
@@ -42,6 +47,7 @@ public class LoginActivity extends AppCompatActivity {
                 Toast.makeText(LoginActivity.this,"You are logged in",Toast.LENGTH_SHORT).show();
                 Intent i = new Intent(LoginActivity.this, HomeActivity.class);
                 startActivity(i);
+                finish();
             }
             else{
                 Toast.makeText(LoginActivity.this,"Please Login",Toast.LENGTH_SHORT).show();
@@ -50,27 +56,54 @@ public class LoginActivity extends AppCompatActivity {
 
         btnSignIn.setOnClickListener( v -> signInValidation());
 
-        tvSignUp.setOnClickListener(v -> {
+        textViewvSignUp.setOnClickListener(v -> {
             Intent intSignUp = new Intent(LoginActivity.this, MainActivity.class);
             startActivity(intSignUp);
+        });
+
+        forgotpassword.setOnClickListener( v -> {
+            final EditText resetMail = new EditText(v.getContext());
+            final AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+            passwordResetDialog.setTitle("Reset Password ?");
+            passwordResetDialog.setMessage("Enter Your Email To Received Reset Link.");
+            passwordResetDialog.setView(resetMail);
+
+            passwordResetDialog.setPositiveButton("Yes", (dialog, which) -> {
+                // extract the email and send reset link
+                String mail = resetMail.getText().toString();
+                mFirebaseAuth.sendPasswordResetEmail(mail).addOnSuccessListener(aVoid -> Toast.makeText(LoginActivity.this, "Reset Link Sent To Your Email.", Toast.LENGTH_SHORT).show()).addOnFailureListener(e -> Toast.makeText(LoginActivity.this, "Error ! Reset Link is Not Sent" + e.getMessage(), Toast.LENGTH_SHORT).show());
+
+            });
+
+            passwordResetDialog.setNegativeButton("No", (dialog, which) -> {
+                // close the dialog
+            });
+
+            passwordResetDialog.create().show();
+
+
         });
     }
     public void signInValidation(){
         String email = emailId.getText().toString();
-        String pwd = password.getText().toString();
+        String password = passwordId.getText().toString();
         if(email.isEmpty()){
             emailId.setError("Please enter email id");
             emailId.requestFocus();
-        }
-        else  if(pwd.isEmpty()){
-            password.setError("Please enter your password");
-            password.requestFocus();
-        }
-        else  if(email.isEmpty() && pwd.isEmpty()){
+        } else if(!email.matches("^[a-zA-Z]+([._+-]{0,1}[a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}+(?:\\.[a-z]{2,}){0,1}$")) {
+            emailId.setError("Please enter valid email id");
+            emailId.requestFocus();
+        }else  if(password.isEmpty()){
+            passwordId.setError("Please enter your password");
+            passwordId.requestFocus();
+        } else  if(!password.matches("(^(?=.*[A-Z]))(?=.*[0-9])(?=.*[a-z])(?=.*[@*&^%#-*+!]{1}).{8,}$")) {
+            passwordId.setError("Please enter Valid password");
+            passwordId.requestFocus();
+        }else  if(email.isEmpty() && password.isEmpty()){
             Toast.makeText(LoginActivity.this,"Fields Are Empty!",Toast.LENGTH_SHORT).show();
         }
-        else  if(!(email.isEmpty() && pwd.isEmpty())){
-            mFirebaseAuth.signInWithEmailAndPassword(email, pwd).addOnCompleteListener(LoginActivity.this, task -> {
+        else  if(!(email.isEmpty() && password.isEmpty())){
+            mFirebaseAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, task -> {
                 if(!task.isSuccessful()){
                     Toast.makeText(LoginActivity.this,"Login Error, Please Login Again",Toast.LENGTH_SHORT).show();
                 }
