15528282174deaa14ae21624db3320134dc1684a
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-04-27 19:36:37 +0530

    Added:Refactored the code for SharedPreference

--- app/src/main/java/com/example/fundoapp/HomeActivity.java
+++ app/src/main/java/com/example/fundoapp/HomeActivity.java
@@ -1,11 +1,16 @@
 package com.example.fundoapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
+
+import static com.example.fundoapp.LoginActivity.Flag;
+import static com.example.fundoapp.LoginActivity.SHARED_PREFS;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -13,19 +18,22 @@ public class HomeActivity extends AppCompatActivity {
     FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         btnLogout = findViewById(R.id.logout);
 
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intToMain = new Intent(HomeActivity.this, MainActivity.class);
-                startActivity(intToMain);
-            }
+        btnLogout.setOnClickListener(v -> {
+            FirebaseAuth.getInstance().signOut();
+            SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean(Flag,false);
+            editor.apply();
+            Intent intToMain = new Intent(HomeActivity.this, LoginActivity.class);
+            startActivity(intToMain);
+            finish();
         });
     }
 }
