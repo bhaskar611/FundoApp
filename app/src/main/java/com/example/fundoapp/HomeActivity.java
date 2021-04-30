7839f554931da275dc696e489f8452145c456533
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-04-30 15:55:13 +0530

    Added:Refactored Home Acctivity

--- app/src/main/java/com/example/fundoapp/HomeActivity.java
+++ app/src/main/java/com/example/fundoapp/HomeActivity.java
@@ -3,9 +3,18 @@ package com.example.fundoapp;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -17,23 +26,71 @@ public class HomeActivity extends AppCompatActivity {
     Button btnLogout;
     FirebaseAuth mFirebaseAuth;
     private FirebaseAuth.AuthStateListener mAuthStateListener;
+    private DrawerLayout drawer;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        btnLogout = findViewById(R.id.logout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this::onNavigationItemSelected);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new NotesFragment()).commit();
+            navigationView.setCheckedItem(R.id.note);
+        }
+    }
 
-        btnLogout.setOnClickListener(v -> {
-            FirebaseAuth.getInstance().signOut();
-            SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
-            SharedPreferences.Editor editor = sharedPreferences.edit();
-            editor.putBoolean(Flag,false);
-            editor.apply();
-            Intent intToMain = new Intent(HomeActivity.this, LoginActivity.class);
-            startActivity(intToMain);
-            finish();
-        });
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.note:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new NotesFragment()).commit();
+                break;
+            case R.id.archive:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Archive_Fragment()).commit();
+                break;
+            case R.id.remainder:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ReminderFragment()).commit();
+                break;
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean(Flag,false);
+                editor.apply();
+                finish();
+                Intent intToMain = new Intent(HomeActivity.this, LoginActivity.class);
+                startActivity(intToMain);
+                finish();
+
+                break;
+                case R.id.delete:
+                    Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
+                     break;
+            case R.id.help:
+                Toast.makeText(this, "Send", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
