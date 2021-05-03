ddb7a26d506d65561bcf0a0f3488f0f9027732af
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-05-03 17:45:40 +0530

    Added:refactored SplashActivity

--- app/src/main/java/com/example/fundoapp/SplashActivity.java
+++ app/src/main/java/com/example/fundoapp/SplashActivity.java
@@ -1,42 +1,42 @@
 package com.example.fundoapp;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import static com.example.fundoapp.LoginActivity.Flag;
-import static com.example.fundoapp.LoginActivity.SHARED_PREFS;
-
 
 public class SplashActivity extends AppCompatActivity {
 
     private boolean isLoggedIn;
+    SharedPreference sharedPreference;
+
+    public SplashActivity(){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        sharedPreference = new SharedPreference(this);
 
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams
                 .FLAG_FULLSCREEN);
 
         new Handler().postDelayed(() -> {
-            SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
-            isLoggedIn = sharedPreferences.getBoolean(Flag, false);
+
+            isLoggedIn = sharedPreference.getLoggedIN();
             Intent intent;
              if (isLoggedIn){
                  intent = new Intent(SplashActivity.this, HomeActivity.class);
-                 startActivity(intent);
-                 finish();
              }else{
                  intent = new Intent(SplashActivity.this, LoginActivity.class);
-                 startActivity(intent);
-                 finish();
              }
+            startActivity(intent);
+            finish();
 
 
         },4000);
