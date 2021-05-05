63b5c30b552b5de4dd9c643af570eb5535af9351
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-05-05 12:27:13 +0530

    Added:refactored NotesFragment.java

--- app/src/main/java/com/example/fundoapp/NotesFragment.java
+++ app/src/main/java/com/example/fundoapp/NotesFragment.java
@@ -7,12 +7,34 @@ import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class NotesFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_notes, container, false);
         return inflater.inflate(R.layout.fragment_notes, container, false);
+    }
+
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+
+        super.onActivityCreated(savedInstanceState);
+
+        FloatingActionButton addnote = (FloatingActionButton)getView().findViewById(R.id.addNotes);
+        addnote.setOnClickListener(v -> {
+            Fragment fragment = new AddNotes_Fragment();
+            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, fragment);
+            fragmentTransaction.addToBackStack(null);
+            fragmentTransaction.commit();
+
+        });
+
     }
 }
