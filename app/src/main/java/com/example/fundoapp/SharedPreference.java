7290773b067bf26c7be0603c28317a04f362b9ca
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-05-05 12:38:46 +0530

    Added:refactored SharedPreference

--- app/src/main/java/com/example/fundoapp/SharedPreference.java
+++ app/src/main/java/com/example/fundoapp/SharedPreference.java
@@ -3,35 +3,35 @@ package com.example.fundoapp;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class SharedPreference extends AppCompatActivity {
-    Context context;
-    public SharedPreference(LoginActivity loginActivity) {
-        this.context = loginActivity;
-    }
-
-    public SharedPreference(HomeActivity homeActivity) {
-        this.context = homeActivity;
-    }
-
-    public SharedPreference(SplashActivity splashActivity) {
-        this.context = splashActivity;
-    }
-
+public class SharedPreference  {
+  private Context context;
     public static final String SHARED_PREFS = "sharedPrefs";
     public static final String IS_LOGGED_IN = "Logged_In";
-    SharedPreferences sharedPreferences =context.getSharedPreferences(SHARED_PREFS,Context.MODE_PRIVATE);
-    SharedPreferences.Editor editor = sharedPreferences.edit();
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
+    public SharedPreference(Context context) {
+        this.context = context;
+        sharedPreferences = context.getSharedPreferences(SHARED_PREFS,Context.MODE_PRIVATE);
 
-
+    }
+//    public SharedPreference(LoginActivity loginActivity) {
+//        this.context = loginActivity;
+//    }
+//
+//    public SharedPreference(HomeActivity homeActivity) {
+//        this.context = homeActivity;
+//    }
+//
+//    public SharedPreference(SplashActivity splashActivity) {
+//        this.context = splashActivity;
+//    }
     public void setLoggedIN(boolean value){
+        editor  = sharedPreferences.edit();
         editor.putBoolean(IS_LOGGED_IN,value);
         editor.apply();
-        finish();
-    }
 
+    }
     public boolean getLoggedIN(){
         return sharedPreferences.getBoolean(IS_LOGGED_IN,false);
 
