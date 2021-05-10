7a65333a057f61a985fcc1ad6000b4e39be1f9bd
bhaskar611 <bhaskarnavuluri9@gmail.com>
2021-05-10 10:45:43 +0530

    Added:Adapter file

---
+++ app/src/main/java/com/example/fundoapp/Adapter.java
@@ -0,0 +1,84 @@
+package com.example.fundoapp;
+import android.os.Build;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+//    List<String> titles;
+//    List<String> content;
+    private final ArrayList<Note> notesList;
+
+    public Adapter(ArrayList<Note> notesList){
+        this.notesList = notesList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.notes_layout,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        holder.noteTitle.setText(notesList.get(position).getTitle());
+        holder.noteContent.setText(notesList.get(position).getContent());
+        final int code = getRandomColor();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            holder.mCardView.setCardBackgroundColor(holder.view.getResources().getColor(code,null));
+        }
+
+
+    }
+
+    private int getRandomColor() {
+
+        List<Integer> colorCode = new ArrayList<>();
+        colorCode.add(R.color.gray);
+        colorCode.add(R.color.pink);
+        colorCode.add(R.color.lightgreen);
+        colorCode.add(R.color.skyblue);
+        colorCode.add(R.color.color1);
+        colorCode.add(R.color.color2);
+        colorCode.add(R.color.color3);
+        colorCode.add(R.color.color4);
+        colorCode.add(R.color.color5);
+        colorCode.add(R.color.green);
+
+        Random randomColor = new Random();
+        int number = randomColor.nextInt(colorCode.size());
+        return colorCode.get(number);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return notesList.size();
+    }
+
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView noteTitle,noteContent;
+        View view;
+        CardView mCardView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            noteTitle = itemView.findViewById(R.id.title);
+            noteContent = itemView.findViewById(R.id.content);
+            mCardView = itemView.findViewById(R.id.notecard);
+            view = itemView;
+        }
+    }
+}
